a8ccd19786ba3ebbb1b57c3f1af41e2c22c0da6d
cvpr20222992 <93644815+cvpr20222992@users.noreply.github.com>
2021-11-07 23:12:02 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0
 export PYTHONWARNINGS="ignore"
 
 # resnet18, resnet50Health Screening Form - MC1
@@ -13,12 +13,6 @@ export w_decay=1e-5
 export epochs=100
 export batchsize=32
 
-python train.py --net ${NET} --n_classes ${N_CLASSES} --path ${path} --data_base ${data} --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --nh 0.5 --warmup 5 --denoise --alpha 0.5 --relabel --tau 0.04
-
-sleep 100
-python train.py --net ${NET} --n_classes ${N_CLASSES} --path ${path} --data_base ${data} --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --nh 0.5 --warmup 5 --denoise --alpha 0.5 --relabel --tau 0.04
-
-sleep 100
 python train.py --net ${NET} --n_classes ${N_CLASSES} --path ${path} --data_base ${data} --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --nh 0.5 --warmup 5 --denoise --alpha 0.5 --relabel --tau 0.04
 
 
